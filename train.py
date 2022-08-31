0032ef441eab9177cc4fe68f7c55c09bac03cd32
Carro <robertmyers@hey.com>
2022-08-31 12:49:54 -0500

    added in new_train, which includes streaming HF data

--- train.py
+++ train.py
@@ -1,31 +1,26 @@
-from memory_efficient_attention_pytorch.transformer import Transformer
-from memory_efficient_attention_pytorch.autoregressive_wrapper import AutoregressiveWrapper
-
-import pdb
-
-import pickle
-import os
-import random
 import gzip
+import os
+import pdb
+import pickle
+import random
+from itertools import chain
+
+import bittensor
+import datasets
 import numpy as np
 import torch
 import torch.optim as optim
-from torch.nn import functional as F
-from torch.utils.data import DataLoader, Dataset as PytorchDataset
-from itertools import chain
-import bittensor
-from tqdm.auto import tqdm
-
-
-from transformers import (
-    AutoConfig,
-    AutoModelForCausalLM,
-    AutoTokenizer,
-    default_data_collator,
-    get_scheduler,
-)
-import datasets
 from datasets import Dataset, DatasetDict, load_dataset
+from torch.nn import functional as F
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset as PytorchDataset
+from tqdm.auto import tqdm
+from transformers import (AutoConfig, AutoModelForCausalLM, AutoTokenizer,
+                          default_data_collator, get_scheduler)
+
+from memory_efficient_attention_pytorch.autoregressive_wrapper import \
+    AutoregressiveWrapper
+from memory_efficient_attention_pytorch.transformer import Transformer
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
