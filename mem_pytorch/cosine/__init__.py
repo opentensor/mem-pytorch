16c8c796bc73e5a3a7d7f1bec1e13e4ee1e0183c
Carro <robertmyers@hey.com>
2022-10-26 12:08:06 -0500

    updated import path

--- mem_pytorch/cosine/__init__.py
+++ mem_pytorch/cosine/__init__.py
@@ -1,3 +1,3 @@
 # from mem_pytorch.cosine import flash_cosine_sim_attention, plain_cosine_sim_attention, l2norm_tensors, debug
-from .flash_cosine_sim_attention import flash_cosine_sim_attention, plain_cosine_sim_attention, l2norm_tensors, debug
+from .flash_cosine_sim_attention import flash_cosine_sim_attention, plain_cosine_sim_attention, l2norm_tensors
 from .transformer import CosineSimCausalTransformer
