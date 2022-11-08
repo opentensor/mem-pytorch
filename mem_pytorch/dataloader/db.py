a25930d15aa95bdbecff593433eb872348a54d60
Carro <robertmyers@hey.com>
2022-11-08 17:07:15 -0600

    adding db and dataloader

--- mem_pytorch/dataloader/db.py
+++ mem_pytorch/dataloader/db.py
@@ -30,7 +30,7 @@ def get_args_parser():
 
 
 
-def db_loader_worker(split, fpath, max_seq_len, tokenizer_path):
+def db_loader_worker(tokenizer, max_seq_len, data, compressor, path):
     """
      Reads a single Pile file and writes it to a DB. This worker is used in multiprocessing.
     :param fpath: Path to the Pile file.
@@ -38,28 +38,8 @@ def db_loader_worker(split, fpath, max_seq_len, tokenizer_path):
     :param tokenizer_path: SentencePiece model path.
     """
 
-    logging.warning(f"Starting {fpath}\n")
-    tokenizer = bt.tokenizer()
-    train_dataset = load_dataset("the_pile", split=split)
-
-    # tokenizer = spm.SentencePieceProcessor()
-    # tokenizer.load(tokenizer_path)
-
-
-    # Create a DB file for the Pile file.
-    con = sqlite3.connect(f"/home/ubuntu/mem-pytorch/db/{split}.db")
-    curr = con.cursor()
-    # Create the DB table. This table will store the data. This table has three columns: idx, tokens, and dataset name.
-    # idx is the index of the sentence in the Pile file. tokens is the tokenized sentence. dataset_name is the name of
-    create_cmd = "CREATE TABLE rows (idx INT PRIMARY KEY, dataset TEXT, tokens BLOB)"
-    curr.execute(create_cmd)
-    insert_cmd = "INSERT INTO rows VALUES (?, ?, ?)"
-
-    dctx = zstandard.ZstdDecompressor()
-    compressor = zstandard.ZstdCompressor()
-
     # Read the Pile file.
-    def encode(examples, idx):
+    def encode(examples):
         dataset_name = examples["meta"]['pile_set_name']
         example_length = len(examples["text"])
         tokens = tokenizer(
@@ -72,56 +52,13 @@ def db_loader_worker(split, fpath, max_seq_len, tokenizer_path):
         compressed_tokens = compressor.compress(
             tokens.encode("ASCII")
         )
-
-        curr.execute(insert_cmd, (idx, dataset_name, compressed_tokens))
+        return (dataset_name, compressed_tokens)
+        # curr.execute(insert_cmd, (idx, dataset_name, compressed_tokens))
          
 
-    for idx, example in tqdm(enumerate(train_dataset), total=len(train_dataset)):
-        encode(example, idx)
-        if idx % 1000 == 0:
-            # logging.warning(f"Processed {idx} examples\n")
-            con.commit()
-    con.commit()
-    con.close()
-    logging.warning(f"Finished {fpath}\n")
+    return encode(data)
+
     
-    # idx = 0
-    # lines = 0
-    # with open(fpath, "rb") as fh:
-    #     dctx = zstandard.ZstdDecompressor()
-    #     stream_reader = dctx.stream_reader(fh)
-    #     text_stream = io.TextIOWrapper(stream_reader, encoding="utf-8")
-    #     compressor = zstandard.ZstdCompressor()
-    #     a = time.time()
-    #     for line in text_stream:
-    #         obj = json.loads(line)
-    #         text = obj["text"].strip()
-    #         dataset = obj["meta"]["pile_set_name"]
-    #         ids = [tokenizer.PieceToId("[eod]")] + tokenizer.EncodeAsIds(text)
-
-
-    #         if len(ids) > 2:
-    #             # Split sentences into chunks of max_seq_len.
-    #             for s in range(0, len(ids), max_seq_len):
-    #                 e = s + max_seq_len
-    #                 tokens = " ".join(str(x) for x in ids[s:e])
-    #                 # Compress the tokens
-    #                 compressed_tokens = compressor.compress(
-    #                     tokens.encode(encoding="ASCII")
-    #                 )
-    #                 curr.execute(insert_cmd, (idx, dataset, compressed_tokens))
-    #                 idx += 1
-    #         lines += 1
-    #         if lines % 10000 == 0:
-    #             logging.warning(
-    #                 f"Finished lines {lines} to produce rows {idx} of {fpath} timestamp {datetime.now()}\n"
-    #             )
-    #             con.commit()
-    #     con.commit()
-    #     con.close()
-    #     logging.warning(f"Finished {fpath} with {lines}\n")
-
-
 
 
 def load_db(stage, path, max_seq_len, tokenizer_path):
@@ -134,55 +71,61 @@ def load_db(stage, path, max_seq_len, tokenizer_path):
     :param max_seq_len: Maximum sequence length.
     :param tokenizer_path: SentencePiece model path.
     """
-    args = [(stage, path, max_seq_len, tokenizer_path)]
-    # if stage == "train":
-    #     # Arguments for multiprocessing workers.
-    #     dpath = os.path.join(path, "train")
-    #     paths = [os.path.join(dpath, x) for x in os.listdir(dpath) if x.endswith("zst")]
-    #     args = [(fpath, max_seq_len, tokenizer_path) for fpath in paths]
+    train_dataset = load_dataset("the_pile", split=stage)
 
-
-    #     # delete the old DB files.
-    #     [
-    #         os.remove(os.path.join(dpath, x))
-    #         for x in os.listdir(dpath)
-    #         if x.endswith("db")
-    #     ]
-    # elif stage == "val":
-    #     # Arguments for multiprocessing workers.
-    #     dpath = os.path.join(path, "val")
-    #     paths = [os.path.join(dpath, x) for x in os.listdir(dpath) if x.endswith("zst")]
-    #     args = [(fpath, max_seq_len, tokenizer_path) for fpath in paths]
-
-
-    #     # delete the old DB files.
-    #     [
-    #         os.remove(os.path.join(dpath, x))
-    #         for x in os.listdir(dpath)
-    #         if x.endswith("db")
-    #     ]
-    # elif stage == "test":
-    #     # Arguments for multiprocessing workers.
-    #     dpath = os.path.join(path, "test")
-    #     paths = [os.path.join(dpath, x) for x in os.listdir(dpath) if x.endswith("zst")]
-    #     args = [(fpath, max_seq_len, tokenizer_path) for fpath in paths]
-
-
-        # delete the old DB files.
-        # [
-        #     os.remove(os.path.join(dpath, x))
-        #     for x in os.listdir(dpath)
-        #     if x.endswith("db")
-        # ]
 
     os.remove(os.path.join(f"/home/ubuntu/mem-pytorch/db/{stage}.db"))
     # Create the DB file.
     os.system(f"touch /home/ubuntu/mem-pytorch/db/{stage}.db")
 
-    with Pool(processes=os.cpu_count()) as p:
-        p.map(db_loader_worker, (stage, path, max_seq_len, tokenizer_path))
+    tokenizer = bt.tokenizer()
 
-    # pool = Pool()
+
+    # tokenizer = spm.SentencePieceProcessor()
+    # tokenizer.load(tokenizer_path)
+
+
+    # Create a DB file for the Pile file.
+    con = sqlite3.connect(f"/home/ubuntu/mem-pytorch/db/{stage}.db")
+    curr = con.cursor()
+    # Create the DB table. This table will store the data. This table has three columns: idx, tokens, and dataset name.
+    # idx is the index of the sentence in the Pile file. tokens is the tokenized sentence. dataset_name is the name of
+    create_cmd = "CREATE TABLE rows (idx INT PRIMARY KEY, dataset TEXT, tokens BLOB)"
+    curr.execute(create_cmd)
+    insert_cmd = "INSERT INTO rows VALUES (?, ?, ?)"
+
+    dctx = zstandard.ZstdDecompressor()
+    compressor = zstandard.ZstdCompressor()
+
+    # with Pool(processes=os.cpu_count()) as p:
+    #     p.starmap(db_loader_worker, args)
+    # args = (tokenizer, max_seq_len, path)
+    pool = Pool(os.cpu_count())
+
+    #  show an example of how to chunk the train_dataset into smaller chunks of size 1000
+    jobs = []
+    for i in range(0, len(train_dataset), 1000):
+        chunk = train_dataset[i:i+1000]
+        for data in tqdm(chunk):
+            jobs.append(pool.apply_async(db_loader_worker, args=(tokenizer, max_seq_len, data, compressor, path)))
+        
+        results = [job.get() for job in jobs]
+            
+
+        # #  create a list of tuples, where each tuple is a chunk of the train_dataset
+        # args = [(tokenizer, max_seq_len, chunk, compressor, path)]
+        # #  map the db_loader_worker function to the list of tuples
+        # #  this will return a list of tuples, where each tuple is a chunk of the train_dataset
+        # #  with the tokens encoded
+        # results = pool.starmap(db_loader_worker, args)
+        #  insert the results into the database
+        for result in results:
+            curr.execute(insert_cmd, (i, result[1], result[2]))
+        #  commit the changes to the database
+        con.commit()
+
+
+    
     # pool.map(db_loader_worker, args)
     # pool.close()
 
