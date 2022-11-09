fcbfa855833de38fe962ef1520163a1e7316eb67
Carro <robertmyers@hey.com>
2022-11-08 18:08:25 -0600

    adding db and dataloader

--- mem_pytorch/dataloader/db.py
+++ mem_pytorch/dataloader/db.py
@@ -56,8 +56,9 @@ def db_loader_worker(idx, max_seq_len, data, path):
         # compressed_tokens = compressor.compress(
         #     tokens.encode("ASCII")
         # )
-        compressed_tokens = tokens
-        return (idx, dataset_name, compressed_tokens)
+        compressed_tokens = tokens['input_ids']
+        compressed_attention_mask = tokens['attention_mask']
+        return (idx, dataset_name, compressed_tokens, compressed_attention_mask)
         # curr.execute(insert_cmd, (idx, dataset_name, compressed_tokens))
          
 
@@ -94,27 +95,15 @@ def load_db(stage, path, max_seq_len, tokenizer_path):
     curr = con.cursor()
     # Create the DB table. This table will store the data. This table has three columns: idx, tokens, and dataset name.
     # idx is the index of the sentence in the Pile file. tokens is the tokenized sentence. dataset_name is the name of
-    create_cmd = "CREATE TABLE rows (idx INT PRIMARY KEY, dataset TEXT, tokens BLOB)"
+    create_cmd = "CREATE TABLE rows (idx INT PRIMARY KEY, dataset TEXT, input_ids BLOB, attention_nask BLOB)"
     curr.execute(create_cmd)
     insert_cmd = "INSERT INTO rows VALUES (?, ?, ?)"
 
     dctx = zstandard.ZstdDecompressor()
 
-    # with Pool(processes=os.cpu_count()) as p:
-    #     p.starmap(db_loader_worker, args)
-    # args = (tokenizer, max_seq_len, path)
     pool = Pool(os.cpu_count())
 
-    #  show an example of how to chunk the train_dataset into smaller chunks of size 1000
     jobs = []
-
-    # function that generates a strig with the length of 8 that is random
-    def random_string(string_length=8):
-        """Returns a random string of length string_length."""
-        random = str(uuid.uuid4())
-        random = random.upper()
-        random = random.replace("-", "")
-        return random[0:string_length]
 
     idx = 0
     for data in tqdm(train_dataset, total=len(train_dataset)):
@@ -126,45 +115,11 @@ def load_db(stage, path, max_seq_len, tokenizer_path):
             if idx != 0:
                 for job in jobs:
                     # pdb.set_trace()
-                    index, dataset_name, compressed_tokens = job.get()
-                    curr.execute(insert_cmd, (index, dataset_name, compressed_tokens))
+                    index, dataset_name, compressed_tokens, compressed_attention_mask = job.get()
+                    curr.execute(insert_cmd, (index, dataset_name, compressed_tokens, compressed_attention_mask))
                 con.commit()
         idx += 1
     
-
-
-    # for i in range(0, len(train_dataset), chunk_size):
-    # for data in tqdm(enumerate(train_dataset), total=len(train_dataset)):
-    #     pdb.set_trace()
-    #     # text_chunk = train_dataset['text'][i:i+chunk_size]
-    #     # meta_chunk = train_dataset['meta'][i:i+chunk_size]
-    #     # chunk = {'text': text_chunk, 'meta': meta_chunk}
-    #     for data in tqdm(text_chunk):
-    #         pdb.set_trace()
-    #         jobs.append(pool.apply_async(db_loader_worker, args=(max_seq_len, data, path)))
-        
-    #     results = [job.get() for job in jobs]
-            
-
-    #     # #  create a list of tuples, where each tuple is a chunk of the train_dataset
-    #     # args = [(tokenizer, max_seq_len, chunk, compressor, path)]
-    #     # #  map the db_loader_worker function to the list of tuples
-    #     # #  this will return a list of tuples, where each tuple is a chunk of the train_dataset
-    #     # #  with the tokens encoded
-    #     # results = pool.starmap(db_loader_worker, args)
-    #     #  insert the results into the database
-    #     for result in results:
-    #         curr.execute(insert_cmd, (i, result[1], result[2]))
-    #     #  commit the changes to the database
-    #     con.commit()
-
-
-    
-    # pool.map(db_loader_worker, args)
-    # pool.close()
-
-    # with ThreadPoolExecutor(max_workers=os.cpu_count()) as executor:
-    #     executor.map(db_loader_worker, args)
 
 
 
